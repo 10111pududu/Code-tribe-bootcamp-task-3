8cbb568204408edc8feccfdc3d7b16644c62e1fe
10111pududu <68174116+10111pududu@users.noreply.github.com>
2024-04-24 11:05:30 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,5 @@
+ document.write(" <h1>BIO ABOUT MYSELF</h1>");
+ document.write("<p><span>WELCOME! I am a self taught front-end Developer passionate about building beautiful and user-friendly websites using HTML, CSS, and JavaScript.</span></p>");
+ document.write("<h2>SKILLS</h2>");
+ document.write("</<ul><li>HTML5</li><li>CSS</li><li>JavaScript</li><li>Resposive Web design</li><li>React<li>Grid Layout</li></ul>");
+ 
